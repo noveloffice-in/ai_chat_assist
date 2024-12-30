fdbd7fd04d6fdfd3ca8b98408ed75174f9b9f58d
prabhu797 <prabhudesai007@gmail.com>
2024-12-30 11:07:42 +0530

    fix: fixed before uninstall script

--- ai_chat_assist/scripts/before_uninstall.py
+++ ai_chat_assist/scripts/before_uninstall.py
@@ -1,6 +1,4 @@
 import subprocess
-import os
 
 def delete_supervisor_file_for_node_app():
-    subprocess.run("cd /etc/supervisor/conf.d")
-    subprocess.run("sudo rm -rf ai_chat_assist.conf")
+    subprocess.run("sudo rm -rf /etc/supervisor/conf.d/ai_chat_assist.conf", shell=True)
