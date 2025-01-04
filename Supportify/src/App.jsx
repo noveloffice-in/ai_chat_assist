b585080392dc90bec7d4d2dd85c28cbbd3812248
prabhu797 <prabhudesai007@gmail.com>
2025-01-04 13:13:58 +0530

    chore: add login feature with frappe react sdk

--- Supportify/src/App.jsx
+++ Supportify/src/App.jsx
@@ -1,43 +1,29 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import io from 'socket.io-client';
 
-const socket = io.connect("https://erp.novelofficedfw.com");
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-	const [room, setRoom] = useState("");
-	const [msg, setMsg] = useState("");
-	const [msgReceived, setMsgReceived] = useState("");
-
-	const sendMessage = () => {
-		socket.emit('sendMessage', { msg, room, username: "Agent Falcon" });
-	}
-
-	useEffect(() => {
-		socket.on("receive_message", (data) => {
-			// console.log('Recevived Message = ', data);
-			setMsgReceived(data.msg);
-		});
-	}, [socket])
-
-	const joinRoom = () => {
-		if (room !== "") {
-			socket.emit("join_room", { room, username: "Agent Falcon" });
-		}
-	}
 
 	return (
-		<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-			<div>
-				<input type="text" placeholder='Room number' value={room} onChange={(e) => { setRoom(e.target.value) }} className='inputMsg' />
-				<button onClick={joinRoom} type="button">Join Room</button>
-			</div>
-			<div>
-				<input type="text" placeholder='Message....' value={msg} onChange={(e) => { setMsg(e.target.value) }} className='inputMsg' />
-				<button onClick={sendMessage} type="button">Send Message</button>
-				<h3> Message: {msgReceived}</h3>
-			</div>
-		</div>
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route
+						path="/dashboard"
+						element={
+							<ProtectedRoute>
+								<Dashboard />
+							</ProtectedRoute>
+						}
+					/>
+					<Route path="/" element={<Navigate to="/dashboard" />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	)
 }
 
